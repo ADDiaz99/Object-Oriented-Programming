95cbb134eff27405b2e3ce520bdc7f07565f549e
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-24 18:07:24 -0500

    added documentation

---
+++ Documentation/define_class.py
@@ -0,0 +1,17 @@
+'''
+En una clase Python, se pueden definir tanto variables como métodos. A diferencia de Java, en Python puede definir cualquier número de clases públicas por archivo de origen (o module). Por lo tanto, un módulo en Python puede considerarse similar a un paquete en Java.
+
+En Python, las clases se definen utilizando la sentencia class. La sentencia class tiene el formato siguiente:
+'''
+
+class name (superclasses): statement
+
+#Or
+
+class name (superclasses): 
+    assignment
+    .
+    .
+    function
+    .
+    .
