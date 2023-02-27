2d10cf587d1f3042cd8ef4729ab3a65167b1a32b
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-27 17:41:54 -0500

    added constructor classes to Python and PHP

--- Python/main.py
+++ Python/main.py
@@ -1,24 +1,22 @@
 from car import *
+from account import Account
 
 
 if __name__ == "__main__":
     print('Hola Python!')
 
-    car = Car()
-    car.license = 'TRA456'
-    car.driver = 'Martin Fierro'
+    car = Car("TRA456", Account("Martin Fierro", "1204572314"))
     car.passenger = 3
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = 'DWA789'
-    car2.driver = 'Jack Daniels'
+    car2 = Car("DWA789", Account("Jack Daniels","10245877568"))
     car2.passenger = 2
     print(vars(car2))
+    print(vars(car2.driver))
     
-    car3 = Car()
-    car3.license = 'EYY469'
-    car3.driver = 'Armando Mendoza'
+    car3 = Car("EYY469", Account("Armando Mendoza","14526354566"))
     car3.passenger = 4
     print(vars(car3))
+    print(vars(car3.driver))
     
