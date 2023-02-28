1fea7c04bc6aaf42e3161c9de47c83dd9b7183ee
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-27 23:26:10 -0500

    finished java classes

--- Java/Driver.java
+++ Java/Driver.java
@@ -1,6 +1,6 @@
 class Driver extends Account{
     public Driver(String name, String document, String email, String password){
-        super(name, document, email, password);
+        super(name, document);
 
     }
 
--- Java/Main.java
+++ Java/Main.java
@@ -1,13 +1,16 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Java!");
+        System.out.println("Hola Mundo");
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
 
-        Car car = new Car("PES213", new Account("Pedro Perez","123456421","pedro@hotmail.com","5456784"));
-        car.passenger = 4;
-        car.printDataCar();
-        System.out.println("Passengers: " + car.passenger);
-        
+        UberVan uberVan = new UberVan("FGH345", new Account("Andres Herrera", "AND123"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
+
+        /*Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
+        car2.passegenger = 3;
+        car2.printDataCar();*/
     }
-    
-
 }
